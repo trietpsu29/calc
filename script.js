4a02c4275de0a38110bdc2ce709f22e29a6d3a17
trietpsu29 <112594221+trietpsu29@users.noreply.github.com>
2025-09-24 18:37:46 +0700

    Implement basic math operators
    Implemented add, substract, multiply, divide two numbers functions.

---
+++ script.js
@@ -0,0 +1,12 @@
+function addNumbers(a, b) {
+  return a + b;
+}
+function subtractNumbers(a, b) {
+  return a - b;
+}
+function multiplyNumbers(a, b) {
+  return a * b;
+}
+function divideNumbers(a, b) {
+  return a / b;
+}
