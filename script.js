601a2c07049c7b4f9e4295c6460b05150c542707
trietpsu29 <112594221+trietpsu29@users.noreply.github.com>
2025-09-27 17:29:43 +0700

    Replace "Clear" with "AC"
    "AC" is more common on calculators than "Clear".

--- script.js
+++ script.js
@@ -15,8 +15,18 @@ let operandB;
 let operator;
 
 function operate(a, op, b) {
-  if (op == "add") return addNumbers(a, b);
-  if (op == "subtract") return subtractNumbers(a, b);
-  if (op == "multiply") return multiplyNumbers(a, b);
+  if (op == "+") return addNumbers(a, b);
+  if (op == "-") return subtractNumbers(a, b);
+  if (op == "x") return multiplyNumbers(a, b);
   return divideNumbers(a, b);
 }
+function populate(val) {
+  const display = document.querySelector(".display");
+  operandA = display.textContent + val;
+  display.textContent = operandA;
+}
+const btn0 = document.querySelector(".zero");
+btn0.addEventListener("click", () => { populate("0") });
+const btn1 = document.querySelector(".one");
+btn1.addEventListener("click", () => { populate("1") });
+
