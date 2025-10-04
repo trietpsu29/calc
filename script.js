41138d2f00bc9807aab7dbb405aa1ff17a4af666
trietpsu29 <112594221+trietpsu29@users.noreply.github.com>
2025-10-04 10:31:22 +0700

    Implement basic calculation function
    User can now perform calculations, each operator processes exactly two
    operands, ensuring predictable and step-by-step evaluation.

--- script.js
+++ script.js
@@ -10,28 +10,51 @@ function multiplyNumbers(a, b) {
 function divideNumbers(a, b) {
   return a / b;
 }
-let operandA = "0";
-let operandB = "";
-let operator
-const display = document.querySelector(".display");
 
 function operate(a, op, b) {
+  if (b == "") return display.textContent;
+  if (a == "") return b;
+  a = Number(a);
+  b = Number(b);
   if (op == "+") return addNumbers(a, b);
   if (op == "-") return subtractNumbers(a, b);
   if (op == "x") return multiplyNumbers(a, b);
   return divideNumbers(a, b);
 }
+
+let operandA = "";
+let operandB = "";
+let operator = "";
+const display = document.querySelector(".display");
+
 function populate(val) {
-  if(operandB=="") display.textContent = val;
+  if (operandB == "") display.textContent = val;
   else display.textContent += val;
   operandB = display.textContent;
 }
 const btnNum = document.querySelectorAll(".num");
-btnNum.forEach((btn)=> {
+btnNum.forEach((btn) => {
   btn.addEventListener("click", (e) => {
-  populate(e.target.textContent);
-})
-})
-
-
-
+    populate(e.target.textContent);
+  });
+});
+const btnOp = document.querySelectorAll(".op");
+btnOp.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    operandA = operate(operandA, operator, operandB);
+    operator = e.target.textContent;
+    operandB = "";
+    display.textContent = operandA;
+  });
+});
+const btnFunc = document.querySelectorAll(".fn");
+btnFunc.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    if (e.target.textContent == "AC") {
+      operandA = "";
+      operandB = "";
+      operator = "";
+      display.textContent = "0";
+    }
+  });
+});
